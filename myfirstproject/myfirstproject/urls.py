5f65835f5f6ac4b607c7d39c5f29da7ef7f5ca0e
mohd-maruf-kureshi-2828 <mohamedmarufkureshikureshi@gmail.com>
2025-10-26 00:35:47 +0530

    Update

--- myfirstproject/myfirstproject/urls.py
+++ myfirstproject/myfirstproject/urls.py
@@ -23,9 +23,6 @@ from .import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home,name="home"),
-    path('about/',views.about,name="about"),
-    path('contact/',views.contact,name="contact"),
     path('myapp/',include('myapp.urls')),
 
 
